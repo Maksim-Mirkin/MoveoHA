2efbee1086e16b53690f82f031207c40e259de0f
Maksim-Mirkin <maksim.mirkin@gmail.com>
2025-11-09 16:48:46 +0200

    Initial commit

---
+++ src/main/java/com/moveo/ha/MoveoHomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.moveo.ha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveoHomeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoveoHomeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moveo/ha/MoveoHomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveo.ha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveoHomeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
